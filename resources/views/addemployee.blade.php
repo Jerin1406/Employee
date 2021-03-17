f7190361bc00489ef02dc2cc529c54eabe085e8f
Jerin K. Thomas <mca519@rcss.com>
2021-03-17 14:42:24 +0530

    Employee laravel

--- resources/views/addemployee.blade.php
+++ resources/views/addemployee.blade.php
@@ -37,6 +37,7 @@
             <div class="col col-12 col-sm-3 col-md-3 col-lg-3">   </div>
             
             <div class="col col-12 col-sm-6 col-md-6 col-lg-6">
+            <table class="table table-borderless">
             <tr>
                 <td>Empname</td>
                 <td><input type="text" class="form-control"></td>
@@ -73,7 +74,8 @@
             <tr>
                 <td></td>
                 <td><button class="btn btn-success">Submit</button></td>
-            </tr>   
+            </tr>  
+            </table> 
             </div>
             <div class="col col-12 col-sm-3 col-md-3 col-lg-3"></div>
         </div>
